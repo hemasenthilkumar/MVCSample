1f2d85ed2aaaadbdfb1716e8a32b5fa0545ccb92
hemasenthilkumar <hema.senthilkumar@outlook.com>
2020-07-11 00:00:08 +0530

    "First commit"

---
+++ MVCSample/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using MVCSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSample.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie();
+            movie.Name = "Shrek!";
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+    }
+}
